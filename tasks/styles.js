4470255315b3eae3b50f4cfa8d59fa35029c7892
Victor Nystad <vnystad@gmail.com>
2014-11-03 14:34:17 +0100

    promises in styles task

--- tasks/styles.js
+++ tasks/styles.js
@@ -2,41 +2,49 @@ var gulp = require('gulp');
 var less = require('less');
 var lessNpmImport = require('less-plugin-npm-import');
 var fs = require('fs');
+var Promise = require('es6-promise').Promise;
 
-function readFile(filename, enc) {
-
-
+function readFile(filename) {
     return new Promise(function(resolve, reject) {
-
-
-        fs.readFile(filename, enc, function(error, result) {
-
+        fs.readFile(filename, 'utf8', function(error, result) {
             if (error) {
                 reject(error);
             } else {
                 resolve(result);
             }
         })
-
     });
 };
 
-gulp.task('styles', function() {
+function writeFile(content) {
+    return new Promise(function(resolve, reject) {
+        fs.writeFile('css/test.css', content, function(error) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        })
+    });
+};
 
-    var arenaCSS = 'amedia-assets-arena/css/design2.src.css';
+function renderLess(lessfile) {
     var options = { plugins: [ lessNpmImport ] };
+    return less.render(lessfile, options)
+        .then(function(css) {
+            return css.css;
+        }, function(error) {
+            return Error(error);
+        });
+};
 
+function handleError(error) {
+    console.error(error);
+};
 
-    //console.log(readFile('css/test.less'));
-    readFile('css/test.less', 'utf8').then(console.log.bind(console));
-
-
-    // less.render('@import (less, reference) "npm://' + arenaCSS + '"; .test:extend(.am-megaMenu-category-title) { background: red }', options)
-    //     .then(function(css) {
-    //         console.log(css.css);
-    //     })
-    //     .catch(function(error) {
-    //         console.error(error);
-    //     });
-
+gulp.task('styles', function() {
+    return readFile('css/test.less')
+        .then(renderLess)
+        .then(writeFile)
+        .catch(handleError);
 });
